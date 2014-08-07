9345950e3dc696cb2c22a0a2a1fb151f1e3af9d3
Stacy J. Prowell <sprowell@gmail.com>
2014-08-07 15:32:25 -0400

    Added a header file for the JSON stuff and started to migrate to using the data structures.

--- examples/json.c
+++ examples/json.c
@@ -1,6 +1,6 @@
 /**
  * @file
- * Eample parser for JSON.
+ * Implementation of the parser for JSON.
  *
  * @verbatim
  * SPSPS
@@ -36,6 +36,7 @@
 
 #define SPSPS_SHORTHAND
 #include "parser.h"
+#include "json.h"
 #include "xstring.h"
 #include <stdlib.h>
 #include <string.h>
@@ -57,12 +58,11 @@ char * printify(unsigned SPSPS_CHAR ch) {
 	return chbuf;
 }
 
-void parse_value(pParser parser);
-void parse_string(pParser parser);
-void parse_number(pParser parser);
-void parse_object(pParser parser);
-void parse_array(pParser array);
-
+json_value * parse_value(pParser parser);
+json_value * parse_string(pParser parser);
+json_value * parse_number(pParser parser);
+json_value * parse_object(pParser parser);
+json_value * parse_array(pParser array);
 
 int main(int argc, char * argv[]) {
 	// If a first argument is provided, it is the file name.  If no first
@@ -87,7 +87,17 @@ int main(int argc, char * argv[]) {
 	exit(0);
 }
 
-void
+/**
+ * Come here to read a JSON value from the stream.  Leading whitespace is
+ * allowed.
+ * @verbatim
+ * value = string | number | object | array
+ *       | "true" | "false" | "null"
+ * @endverbatim
+ * @param parser			The parser object.
+ * @return					The parsed value, or NULL if an error occurs.
+ */
+json_value *
 parse_value(pParser parser) {
 	// Allow whitespace here.
 	spsps_consume_whitespace(parser);
@@ -98,79 +108,163 @@ parse_value(pParser parser) {
 	SPSPS_CHAR ch = spsps_peek(parser);
 	switch (ch) {
 	case '"':
-		parse_string(parser);
+		return parse_string(parser);
 		break;
 	case '-':
-		parse_number(parser);
+		return parse_number(parser);
 		break;
 	case '{':
-		parse_object(parser);
+		return parse_object(parser);
 		break;
 	case '[':
-		parse_array(parser);
+		return parse_array(parser);
 		break;
 	case 't':
 		if (spsps_peek_and_consume(parser, "true")) {
-			puts("true\n");
+			return json_new_boolean(true);
 		} else {
 			ERR(parser, "Saw a value starting with 't' and expected "
 					"true, but did not find that.  Did you forget to "
 					"quote a string?");
-			return;
+			return NULL;
 		}
 		break;
 	case 'f':
 		if (spsps_peek_and_consume(parser, "false")) {
-			puts("false\n");
+			return json_new_boolean(false);
 		} else {
 			ERR(parser, "Saw a value starting with 'f' and expected "
 					"false, but did not find that.  Did you forget to "
 					"quote a string?");
-			return;
+			return NULL;
 		}
 		break;
 	case 'n':
 		if (spsps_peek_and_consume(parser, "null")) {
-			puts("null\n");
+			return json_new_null();
 		} else {
 			ERR(parser, "Saw a value starting with 'n' and expected "
 					"null, but did not find that.  Did you forget to "
 					"quote a string?");
-			return;
+			return NULL;
 		}
 		break;
 	default:
 		// Check for a digit.
 		if (isdigit(ch)) {
-			parse_number(parser);
+			return parse_number(parser);
 		} else {
 			ERR(parser, "Expected to find a value, but instead found "
 					"unexpected character %s, which does not start a "
 					"value.  Did you forget to quote a string?", printify(ch));
-			return;
+			return NULL;
 		}
 	}
 }
 
-void
+/* Private function to convert a character to a nibble.  If the character
+ * is not a valid hex character (upper- or lower-case) then the return value
+ * is 255.  Otherwise it is going to be in the range [0,15].
+ */
+char
+unhex_(char ch) {
+	if (ch >= '0' && ch <= '9') {
+		return ch - '0';
+	} else if (ch >= 'a' && ch <= 'f') {
+		return ch - 'a' + 10;
+	} else if (ch >= 'A' && ch <= 'F') {
+		return ch - 'A' + 10;
+	} else {
+		return 255;
+	}
+}
+
+/**
+ * Come here to parse a quoted string from the stream.  The stream must
+ * be pointing to the open quotation mark.
+ * @verbatim
+ * string = '"' ( CHARACTER | ESCAPE )* '"'
+ * @endverbatim
+ * A `CHARACTER` is any character other than `\` and `"`.  An escape can
+ * be any of `\r`, `\n`, `\t`, or a protected character like `\` and `"`.
+ * It can also be used to join lines if it is the last character on a line,
+ * and it can introduce an 8-bit hex escape (`\x0a`, for instance).
+ * @param parser			The parser.
+ * @return					The parsed string value, or NULL on error.
+ */
+json_value *
 parse_string(pParser parser) {
 	// The first thing in the stream must be the quotation mark.
 	if (! spsps_peek_and_consume(parser, "\"")) {
 		ERR(parser, "Expected to find a quotation mark for a string, "
 				"but instead found %s.", printify(spsps_peek(parser)));
-		return;
+		return NULL;
 	}
 	// Read the rest of the string.
 	mstring str = NULL;
+	SPSPS_CHAR highc, lowc;
+	char high, low;
 	while (! spsps_peek_and_consume(parser, "\"") && !spsps_eof(parser)) {
-		mstr_append(str, spsps_consume(parser));
+		if (spsps_peek_and_consume(parser, "\\")) {
+			// Process an escape.
+			SPSPS_CHAR ch = spsps_consume(parser);
+			switch (ch) {
+			case 'n':
+				str = mstr_append(str, '\n');
+				break;
+			case 'r':
+				str = mstr_append(str, '\r');
+				break;
+			case 't':
+				str = mstr_append(str, '\t');
+				break;
+			case '\r':
+				spsps_peek_and_consume(parser, "\n");
+				break;
+			case '\n':
+				break;
+			case 'x':
+				// Extract the two hexadecimal characters.
+				highc = spsps_consume(parser);
+				lowc = spsps_consume(parser);
+				high = unhex_((char) highc);
+				low = unhex_((char) lowc);
+				if (high > 15) {
+					ERR(parser, "Expected to find two hexadecimal digits "
+							"in an escape (starting with \\x) but "
+							"instead found %s.", printify(highc));
+				}
+				if (low > 15) {
+					ERR(parser, "Expected to find two hexadecimal digits "
+							"in an escape (starting with \\x) but "
+							"instead found %s.", printify(lowc));
+				}
+				str = mstr_append(str, (char) ((high << 4)|low));
+				break;
+			default:
+				str = mstr_append(str, ch);
+				break;
+			}
+		} else {
+			str = mstr_append(str, spsps_consume(parser));
+		}
 	} // Loop over the characters in the string.
 	char * cstring = mstr_cstr(str);
-	printf("string: %s\n", cstring);
-	free(cstring);
+	return json_new_string(cstring);
 }
 
-void
+/**
+ * Come here to parse a number.  The first character in the stream is
+ * expected to be the first character of the number.  This could be a
+ * minus sign, or it could be a digit.  There must be at least one
+ * digit.  The digits are interpreted in base 10, and stored in a
+ * C `int`.
+ * @verbatim
+ * number = '-'? ( '0'..'9' )+
+ * @endverbatim
+ * @param parser			The parser.
+ */
+json_value *
 parse_number(pParser parser) {
 	// Build the number by consuming the digits.
 	bool neg = spsps_peek_and_consume(parser, "-");
@@ -178,43 +272,52 @@ parse_number(pParser parser) {
 	if (! isdigit(spsps_peek(parser))) {
 		ERR(parser, "Expected to find a digit, but instead found %s.",
 				printify(spsps_peek(parser)));
-		return;
+		return NULL;
 	}
 	while (isdigit(spsps_peek(parser))) {
 		value *= 10;
 		value += spsps_consume(parser) - '0';
 	} // Consume all digits.
 	if (neg) value = -value;
-	printf("number: %d\n", value);
+	return json_new_number(value);
 }
 
-void
+/**
+ * Come here to parse a JSON object.  The first character in the stream is
+ * expected to be the opening curly brace for the object.  Objects can be
+ * empty.
+ * @verbatim
+ * object = '{' ( string '=' value )* '}'
+ * @endverbatim
+ * @param parser			The parser.
+ */
+json_value *
 parse_object(pParser parser) {
 	// Arrays start with a curly brace.
 	if (! spsps_peek_and_consume(parser, "{")) {
 		ERR(parser, "Expected to find the start of an object (a curly "
 				"brace), but instead found %s.", printify(spsps_peek(parser)));
-		return;
+		return NULL;
 	}
 	// Now consume a sequence of string equal value pairs.
-	puts("object start");
+	json_object * object = NULL;
 	while (! spsps_eof(parser)) {
 		spsps_consume_whitespace(parser);
 		SPSPS_CHAR ch = spsps_peek(parser);
 		if (ch == '"') {
 			// Start of a pair.  Parse the string.
-			parse_string(parser);
+			json_value * key = parse_string(parser);
 			spsps_consume_whitespace(parser);
 			// Expect an equal sign.
 			if (! spsps_peek_and_consume(parser, "=")) {
 				ERR(parser, "Expected to find an equal sign for a "
 						"string = value pair, but instead found %s.",
 						printify(spsps_peek(parser)));
-				return;
+				return NULL;
 			}
 			spsps_consume_whitespace(parser);
 			// Expect a value.
-			parse_value(parser);
+			json_value * value = parse_value(parser);
 		} else if (ch == '}') {
 			// End of object.
 			break;
@@ -223,12 +326,20 @@ parse_object(pParser parser) {
 					"string = value pair, or the end of the "
 					"object (a closing curly brace), but "
 					"instead found %s.", printify(ch));
-			return;
+			return NULL;
 		}
 	} // Consume all pairs.
 	puts("object end");
 }
 
+/**
+ * Come here to parse a JSON array.  The first character in the stream is
+ * expected to be the opening square bracket.  Arrays can be empty.
+ * @verbatim
+ * array = '[' ( value ( ',' value )* )? ']'
+ * @endverbatim
+ * @param parser				The parser.
+ */
 void
 parse_array(pParser parser) {
 	// Arrays start with a square bracket.
@@ -251,4 +362,233 @@ parse_array(pParser parser) {
 		}
 	} // Consume until closing square bracket.
 	puts("array end");
+}
+
+void
+json_free_value(json_value * value) {
+	json_object * object;
+	json_array * array;
+	switch (value->kind) {
+	case OBJECT:
+		// Free all pairs in the object.
+		object = value->content.objectvlaue;
+		for (int index = 0; index < MAP_SIZE; ++index) {
+			json_object_entry * entry = object->map[index];
+			while (entry != NULL) {
+				free(entry->key);
+				json_free_value(entry->value);
+				entry = entry->next;
+			} // Free all entries with this hash code.
+		} // Free all entries.
+		free(object);
+		free(value);
+		break;
+	case ARRAY:
+		// Free all elements of the array.
+		array = value->content.arrayvalue;
+		for (int index = 0; index < array->size; ++index) {
+			json_free_value(array->array[index]);
+		} // Deallocate all entries.
+		free(array->array);
+		free(array);
+		free(value);
+		break;
+	case STRING:
+		// Free the string.
+		free(value->content.strvalue);
+		free(value);
+		break;
+	case NUMBER:
+	case BOOL:
+	case NOTHING:
+		// These three cases are easy.  Just deallocate the value.
+		free(value);
+		break;
+	}
+}
+
+uint32_t
+hash_string_(unsigned char * str) {
+	uint32_t hash = 5381;
+	uint32_t ch;
+	for (uint32_t ch = *str; ch != 0; str++) {
+		// Use shift to multiply by 32, then subtract once to get
+		// multiplication by 31... my favorite hash multiplier.
+		// No reason.  Then we add the character.  Note that this
+		// preserves some low bits from prior characters, so the
+		// effect of earlier characters never really washes out.
+		hash = (hash << 5) - hash + ch;
+	} // Compute the hash over the string.
+	return hash;
+}
+
+json_object *
+json_object_insert(json_object * object, char * key, json_value * value) {
+	if (object == NULL) {
+		// Allocate an object.
+		object = (json_object *) malloc(sizeof(json_object));
+	}
+	if (key == NULL) {
+		// The key is NULL.  Replace it with the empty string.
+		// We have to allocate, so that the caller can later
+		// deallocate.
+		key = (char *) malloc(sizeof(char)*1);
+		key[0] = 0;
+	}
+	if (value == NULL) {
+		// The value is NULL.  Replace this with an "official"
+		// JSON null.
+		value = (json_value *) malloc(sizeof(json_value));
+		value->kind = NOTHING;
+	}
+	// Compute the hash code for the string.
+	uint32_t hash = hash_string_((unsigned char *) key) % MAP_SIZE;
+	// Get the map entry.
+	json_object_entry * entry = object->map[hash];
+	while (entry != NULL) {
+		if (strcmp(entry->key, key) == 0) {
+			// Free any prior value.
+			json_free_value(entry->value);
+			entry->value = value;
+			return object;
+		}
+		entry = entry->next;
+	} // Find an entry to replace.
+	// Did not find an existing entry; insert the new entry.
+	entry = (json_object_entry *) malloc(sizeof(json_object_entry));
+	entry->key = key;
+	entry->value = value;
+	entry->next = object->map[hash];
+	object->map[hash] = entry;
+	return object;
+}
+
+json_value *
+json_new_boolean(bool flag) {
+	json_value * value = (json_value *) malloc(sizeof(json_value));
+	value->kind = BOOL;
+	value->content.boolvalue = flag;
+	return value;
+}
+
+json_value *
+json_new_null() {
+	json_value * value = (json_value *) malloc(sizeof(json_value));
+	value->kind = NOTHING;
+	return value;
+}
+
+json_value *
+json_new_number(int number) {
+	json_value * value = (json_value *) malloc(sizeof(json_value));
+	value->kind = NUMBER;
+	value->content.numvalue = number;
+	return value;
+}
+
+json_value *
+json_new_string(char * str) {
+	json_value * value = (json_value *) malloc(sizeof(json_value));
+	value->kind = STRING;
+	value->content.strvalue = str;
+	return value;
+}
+
+json_value *
+json_new_array(size_t size) {
+	json_value * value = (json_value *) malloc(sizeof(json_value));
+	value->kind = ARRAY;
+	value->content.arrayvalue = (json_array *) malloc(sizeof(json_array));
+	value->content.arrayvalue->size = size;
+	value->content.arrayvalue->array =
+			(json_value **) malloc(sizeof(json_value *) * size);
+	return value;
+}
+
+json_value *
+json_array_element(json_value * value, size_t index) {
+	if (value == NULL || value->kind != ARRAY) {
+		// There is no array.
+		return NULL;
+	}
+	json_array * array = value->content.arrayvalue;
+	if (index >= array->size) {
+		return NULL;
+	}
+	return array->array[index];
+}
+
+json_value *
+json_get_entry(json_value * value, char * key) {
+	if (value == NULL || value->kind != OBJECT) {
+		return NULL;
+	}
+	if (key == NULL) return NULL;
+	json_object * object = value->content.objectvlaue;
+	// Compute the hash code for the string.
+	uint32_t hash = hash_string_((unsigned char *) key) % MAP_SIZE;
+	// Get the map entry.
+	json_object_entry * entry = object->map[hash];
+	while (entry != NULL) {
+		if (strcmp(entry->key, key) == 0) {
+			return entry->value;
+		}
+		entry = entry->next;
+	} // Find the entry.
+	// Did not find the entry.
+	return NULL;
+}
+
+void
+json_stream(FILE * stream, json_value * value, int depth) {
+	if (stream == NULL) stream = stdout;
+	if (value == NULL) return;
+	switch (value->kind) {
+	if (depth > 0) {
+		for (int advance = 0; advance < depth; ++advance) {
+			fprintf(stream, "  ");
+		} // Indent.
+	}
+	case NOTHING:
+		fprintf(stream, "null\n");
+		break;
+	case BOOL:
+		fprintf(stream, value->content.boolvalue ? "true\n" : "false\n");
+		break;
+	case NUMBER:
+		fprintf(stream, "%d\n", value->content.numvalue);
+		break;
+	case STRING:
+		fprintf(stream, "\"%s\"", value->content.strvalue);
+		break;
+	case ARRAY:
+		fprintf(stream, "[\n");
+		for (int index = 0; index < value->content.arrayvalue->size; ++index) {
+			json_stream(stream, value->content.arrayvalue->array[index], depth+1);
+		} // Write all entries.
+		if (depth > 0) {
+			for (int advance = 0; advance < depth; ++advance) {
+				fprintf(stream, "  ");
+			} // Indent.
+		}
+		fprintf(stream, "]\n");
+		break;
+	case OBJECT:
+		fprintf(stream, "{\n");
+		for (int index = 0; index < MAP_SIZE; ++index) {
+			json_object_entry * entry = value->content.objectvlaue->map[index];
+			while (entry != NULL) {
+				fprintf(stream, "\"%s\" = ", entry->key);
+				json_stream(stream, entry->value, depth+1);
+				entry = entry->next;
+			} // Write all entries off this hash.
+		} // Write all entries.
+		if (depth > 0) {
+			for (int advance = 0; advance < depth; ++advance) {
+				fprintf(stream, "  ");
+			} // Indent.
+		}
+		fprintf(stream, "}\n");
+		break;
+	}
 }
---
+++ examples/json.h
@@ -0,0 +1,192 @@
+#ifndef JSON_H_
+#define JSON_H_
+
+/**
+ * @file
+ * Definitions for the JSON parser.
+ *
+ * @verbatim
+ * SPSPS
+ * Stacy's Pathetically Simple Parsing System
+ * https://github.com/sprowell/spsps
+ *
+ * Copyright (c) 2014, Stacy Prowell
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * @endverbatim
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**********************************************************************
+* Data structures for JSON.
+**********************************************************************/
+
+/// Size of the map.
+#define MAP_SIZE 256
+
+/// The kind of a JSON value.
+typedef enum json_kind_ {
+	OBJECT, ARRAY, NUMBER, STRING, BOOL, NOTHING
+	// Note we use NOTHING instead of NULL, because its reserved.
+} json_kind;
+
+/// A single entry in a JSON object.
+typedef struct json_object_entry_ {
+	char * key;
+	struct json_value_ * value;
+	struct json_object_entry_ * next;
+} json_object_entry;
+
+/// A simple JSON object.
+typedef struct json_object_ {
+	struct json_object_entry_ * map[MAP_SIZE];
+} json_object;
+
+/// A simple JSON array.
+typedef struct json_array_ {
+	size_t size;
+	struct json_value_ ** array;
+} json_array;
+
+/// The content of a JSON value.
+typedef union json_content_ {
+	int numvalue;
+	char * strvalue;
+	bool boolvalue;
+	json_array * arrayvalue;
+	json_object * objectvlaue;
+} json_content;
+
+/// A JSON value.
+typedef struct json_value_ {
+	json_kind kind;
+	json_content content;
+} json_value;
+
+/**********************************************************************
+* JSON manipulators.
+**********************************************************************/
+
+/**
+ * Free a JSON value.  Values can be quite complicated, and this will
+ * free the value and all subordinate values (for an array or object).
+ * @param value			The value to free.
+ */
+void json_free_value(json_value * value);
+
+/**
+ * Compute a hash function over the string.
+ * @param str			The string.
+ * @return				The computed hash.
+ */
+uint32_t hash_string_(unsigned char * str);
+
+/**
+ * Insert an entry into a JSON object.  The object is implemented as
+ * a very simple hash table, with collisions resolved by chaining.
+ * @param object		The object to modify.  Can be NULL.
+ * @param key			The key to insert.
+ * @param value			The value to insert.
+ * @return				The modified object, created iff NULL was given.
+ */
+json_object * json_object_insert(json_object * object, char * key,
+		json_value * value);
+
+/**
+ * Make and return a new JSON Boolean value.
+ * @param flag			The value.
+ * @return				The new JSON value.
+ */
+json_value * json_new_boolean(bool flag);
+
+/**
+ * Make and return a new JSON null value.
+ * @return				The new JSON null value.
+ */
+json_value * json_new_null();
+
+/**
+ * Make and return a new JSON number value.
+ * @param number		The value.
+ * @return				The new JSON value.
+ */
+json_value * json_new_number(int number);
+
+/**
+ * Make and return a new JSON string value.  The string is stored here,
+ * and will be deallocated when this entry is deallocated.
+ * @param str			The value.
+ * @return				The new JSON value.
+ */
+json_value * json_new_string(char * str);
+
+/**
+ * Make and return a new JSON array.  The array size is set, but the
+ * array is not populated.
+ * @param size			The array size.
+ * @return				The new JSON array.
+ */
+json_value * json_new_array(size_t size);
+
+/**
+ * Obtain a pointer to an element of a JSON array.  If the element
+ * does not exist, or the value given is not an array, then NULL is
+ * returned, so watch for that.  The pointer allows the array to be
+ * modified in-place.
+ * @param value			The JSON array.
+ * @param index			The zero-based index of the element.
+ */
+json_value * json_array_element(json_value * value, size_t index);
+
+/**
+ * Obtain a pointer to an entry in a JSON object.  If the entry
+ * does not exist, or the value given is not an object, then NULL is
+ * returned, so watch for that.
+ */
+json_value * json_get_entry(json_value * value, char * key);
+
+/**
+ * Format and print a JSON value to the given stream.
+ * @param stream		The stream to get the data.
+ * @param value			The JSON value.
+ * @param depth			Indentation depth.
+ */
+void json_stream(FILE * stream, json_value * value, int depth);
+
+/**********************************************************************
+* JSON parser.
+**********************************************************************/
+
+/**
+ * Parse a JSON value from the given parser instance.
+ * @param parser		The parser.
+ * @return				The next JSON value parsed.
+ */
+void parse_value(pParser parser);
+
+#endif /* JSON_H_ */
